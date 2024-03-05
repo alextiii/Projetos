992ca599fba0be5abdd9c524a29aaf6a98fbfe8a
Alex Christos Simonis <101401610+alextiii@users.noreply.github.com>
2024-03-05 13:25:40 -0300

    Add files via upload
    Robôs com intuito de integração de informações via Api para alimentação de banco de dados

---
+++ Voll/import requests proa.py
@@ -0,0 +1,126 @@
+import requests
+import pyodbc
+from datetime import datetime
+import urllib3
+import time
+
+
+
+# Desativa os avisos de certificado SSL
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+db_connection_string = (
+    "DRIVER={ODBC Driver 17 for SQL Server};"
+    "SERVER=;"
+    "DATABASE=;"
+    "UID=;"
+    "PWD=;"
+)
+
+db_conn = pyodbc.connect(db_connection_string)
+db_cursor = db_conn.cursor()
+
+api_base_url = ''
+api_key = '5435e5a127810527319801067d61bb98'
+
+today = datetime.now().strftime('%Y-%m-%d')
+
+api_headers = {
+    'voll-api-key': api_key
+}
+
+try:
+    for page in range(1, 50):
+        api_params = {
+            'per': 998,
+            'page': page,
+            'start_date': '2024-01-01' 
+        }
+
+        # Adicione verify=False ao fazer a solicitação para desativar a verificação do certificado SSL
+        response = requests.get(api_base_url, headers=api_headers, params=api_params, verify=False)
+        response.raise_for_status()
+
+        data = response.json().get("items", [])
+
+        for item in data:
+            id_value = item.get("id")
+
+            db_cursor.execute("SELECT COUNT(*) FROM Pbi_voll WHERE id = ?", id_value)
+            if db_cursor.fetchone()[0] > 0:
+                print(f"ID {id_value} já existe no banco de dados. Ignorando inserção.")
+                continue
+
+            customer_started_at_str = item.get("customer_started_at")
+            agent_started_at_str = item.get("agent_started_at")
+            
+            started_at = None
+            agent_started_at = None
+            customer_started_at = None
+            
+            if customer_started_at_str:
+                customer_started_at = datetime.strptime(customer_started_at_str, "%Y-%m-%dT%H:%M:%S.%f%z")
+            
+            if agent_started_at_str:
+                agent_started_at = datetime.strptime(agent_started_at_str, "%Y-%m-%dT%H:%M:%S.%f%z")
+            
+            ended_at_str = item.get("ended_at")
+            ended_at = datetime.strptime(ended_at_str, "%Y-%m-%dT%H:%M:%S.%f%z")
+            
+            if "started_at" in item and item["started_at"]:
+                started_at_str = item.get("started_at")
+                started_at = datetime.strptime(started_at_str, "%Y-%m-%dT%H:%M:%S.%f%z")
+            
+            time_exceeded = item.get("time_exceeded")
+            formatted_time_exceeded = item.get("formatted_time_exceeded")
+            customer_name = item.get("customer_name")
+            cpf = item.get("cpf")
+            campaign_name = item.get("campaign_name")
+            tabulation_name = item.get("tabulation_name")
+            tabulation_group_name = item.get("tabulation_group_name")
+            hook_id = item.get("hook_id")
+            obs = item.get("obs")
+            received_message_count = item.get("received_message_count")
+            sent_message_count = item.get("sent_message_count")
+            total_messages = item.get("total_messages")
+            total_time = item.get("total_time")
+            customer_tmr = item.get("customer_tmr")
+            agent_tmr = item.get("agent_tmr")
+            origin = item.get("origin")
+            agent_name = item.get("agent_name")
+            system = item.get("system")
+            number = item.get("number")
+            
+           
+            insert_query = """
+                INSERT INTO Pbi_voll (
+                    id, started_at, agent_started_at, customer_started_at, ended_at, time_exceeded, formatted_time_exceeded,
+                    customer_name, cpf, campaign_name, tabulation_name, tabulation_group_name, hook_id, obs,
+                    received_message_count, sent_message_count, total_messages, total_time, customer_tmr,
+                    agent_tmr, origin, agent_name, system, number
+                ) VALUES (
+                    ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?
+                )
+            """
+            
+            db_cursor.execute(insert_query, id_value, started_at, agent_started_at, customer_started_at, ended_at,
+                              time_exceeded, formatted_time_exceeded, customer_name, cpf, campaign_name,
+                              tabulation_name, tabulation_group_name, hook_id, obs, received_message_count,
+                              sent_message_count, total_messages, total_time, customer_tmr, agent_tmr, origin,
+                              agent_name, system, number)
+            
+            db_conn.commit()
+            print(f"Inserindo id {id_value} (página {page})")
+
+    print("Dados inseridos com sucesso no banco.")
+
+except requests.exceptions.RequestException as e:
+    print("Erro durante a solicitação da API:", e)
+
+except pyodbc.Error as e:
+    print("Erro ao inserir dados no banco de dados:", e)
+
+finally:
+    db_cursor.close()
+    db_conn.close()
+time.sleep(5)    
---
+++ viavarejo/Analiticoviavarejo.py
@@ -0,0 +1,131 @@
+import requests
+import pyodbc
+from dateutil import parser
+
+def get_new_token():
+    url = "https://api.2clix.com.br/v3/Usuario/login"
+    payload = {
+        "login": "",
+        "senha": ""
+    }
+    headers = {
+        "Content-Type": "application/json"
+    }
+
+    response = requests.post(url, json=payload, headers=headers)
+    if response.status_code == 200:
+        token = response.json().get("token")
+        return token
+    else:
+        raise Exception("Não foi possível obter um novo token.")
+
+
+def fetch_data():
+ 
+    token = get_new_token()
+
+
+    params = {
+        "DtInicio": "2024-01-01",
+        "codTipoFicha": 1
+    }
+
+    headers = {
+        "Authorization": f"Bearer {token}"
+    }
+
+
+    url = ""
+    response = requests.get(url, params=params, headers=headers)
+
+   
+    if response.status_code == 200:
+        data = response.json()
+        return data["result"]
+    else:
+        raise Exception("Erro ao obter dados da API.")
+
+
+def convert_datetime(datetime_str):
+    if datetime_str:
+        datetime_obj = parser.parse(datetime_str)
+        return datetime_obj.strftime("%Y-%m-%d %H:%M:%S")
+    return None
+
+
+def insert_data(data):
+
+    conn = pyodbc.connect(
+        "DRIVER={ODBC Driver 17 for SQL Server};"
+        "SERVER="
+        "DATABASE="
+        "UID=;"
+        "PWD=;"
+    )
+
+    cursor = conn.cursor()
+
+  
+    for item in data:
+        cod_monitoria = str(item.get("COD_MONITORIA"))
+        query = f"SELECT COUNT(*) FROM monitoria2clixviavarejo WHERE COD_MONITORIA = '{cod_monitoria}'"
+        cursor.execute(query)
+        result = cursor.fetchone()
+        count = result[0]
+
+        if count == 0:
+          
+            data_avaliacao = convert_datetime(item.get("data_avaliacao"))
+            data_ligacao = convert_datetime(item.get("DATA_LIGACAO"))
+            data_feedback = convert_datetime(item.get("data_feedback"))
+
+            insert_query = """
+                INSERT INTO monitoria2clixviavarejo (
+                    COD_MONITORIA,
+                    data_avaliacao,
+                    cliente,
+                    campanha,
+                    Superior,
+                    Avaliado,
+                    Avaliador,
+                    Nota,
+                    NOTA_SEM_NCG,
+                    Processo,
+                    DATA_LIGACAO,
+                    data_feedback,
+                    TIPO_FEEDBACK,
+                    CODIGO_GRAVACAO
+                )
+                VALUES (?, CONVERT(datetime2, ?, 126), ?, ?, ?, ?, ?, ?, ?, ?, CONVERT(datetime2, ?, 126), ?, ?, ?)
+            """
+
+            values = (
+                cod_monitoria,
+                data_avaliacao,
+                item.get("cliente"),
+                item.get("campanha"),
+                item.get("Superior"),
+                item.get("Avaliado"),
+                item.get("Avaliador"),
+                str(item.get("Nota")),
+                str(item.get("NOTA_SEM_NCG")),
+                item.get("Processo"),
+                data_ligacao,
+                data_feedback,
+                item.get("TIPO_FEEDBACK"),
+                item.get("CODIGO_GRAVACAO")
+            )
+            cursor.execute(insert_query, values)
+        else:
+            print(f"Dados duplicados: COD_MONITORIA = {cod_monitoria}")
+
+   
+    conn.commit()
+
+    conn.close()
+
+    print("DADOS INSERIDOS")
+
+
+data = fetch_data()
+insert_data(data)
